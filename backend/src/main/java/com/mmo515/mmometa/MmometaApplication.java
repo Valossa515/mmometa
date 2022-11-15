3ccf0704544991e871621f351cfe313ca4937af3
Valossa515 <fe.mmo515@gmail.com>
2022-11-15 12:49:39 -0300

    first commit

---
+++ backend/src/main/java/com/mmo515/mmometa/MmometaApplication.java
@@ -0,0 +1,13 @@
+package com.mmo515.mmometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmometaApplication.class, args);
+	}
+
+}
