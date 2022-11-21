80311cd5bd9949301a490064dcc669bda3feb758
Valossa515 <fe.mmo515@gmail.com>
2022-11-21 15:37:55 -0300

    Mouse hover

--- frontend/src/components/SaleButton/index.tsx
+++ frontend/src/components/SaleButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import addicon from '../../assets/img/add-icon.svg'
 import { BASE_URL } from "../../utils/request";
@@ -11,9 +12,26 @@ function handleClick(){
 }
 
 function SaleButton(){
+    const [isHovering, setIsHovering] = useState(false);
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+      };
+    
+      const handleMouseOut = () => {
+        setIsHovering(false);
+      };
+
     return(
-        <div className="dsmeta-red-btn" onClick={() => handleClick()}>
-            <img src={addicon} alt="Adcionar venda!" /> 
+        <div className="dsmeta-red-btn" onClick={() => handleClick()} 
+        onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            <img src={addicon} />
+            {isHovering &&
+            <div>
+                <p>Adcionar Venda</p>
+            </div>
+            }
+            
         </div>
     )
 }
